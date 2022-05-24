c2a0015ecf144c384b18450dba40b3379591879b
Rowell <rlegaspi311@gmail.com>
2022-05-24 18:08:01 +0800

    Modified js on click and drag but still need to look

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 
 
-const rowNum = 100
-const columnNum = 100
+const rowNum = 20
+const columnNum = 20
 
 const container = document.createElement('div')
 const styleWhole = document.createElement('style')
+const chooseBody = document.querySelector('body')
+
+const title = document.createElement('h1')
+const chooseH1 = document.querySelector('h1')
 
 //CONTAINER STYLES
 // container.style['border'] = 'solid red 3px'
-container.style['width'] = '100%'
-container.style['height'] = '44rem'
+container.style['width'] = '500px'
+container.style['height'] = '500px'
 container.style['display'] = 'flex'
 container.style['flex-direction'] = 'column'
+container.style['background-color'] = 'white'
+container.style['border'] = 'solid black 3px'
+
+
+//TITLE
+title.textContent = 'ETCH-A-SKETCH'
+chooseBody.appendChild(title)
+
+
+//BODY
+chooseBody.style['display'] = 'flex'
+chooseBody.style['flex-direction'] = 'column'
+chooseBody.style['justify-content'] = 'center'
+chooseBody.style['align-items'] = 'center'
+chooseBody.style['margin-top'] = '50px'
+chooseBody.style['background-color'] = '#f0f5f0'
 
 // ADD STYLE IN THE HEAD SECTION
 document.head.appendChild(styleWhole)
@@ -26,7 +46,7 @@ for(let i = 0; i < rowNum; i++){
     const perRow = document.createElement('div')
 
     //ROWS
-    perRow.style['border'] = 'solid #7e05ff 1px'
+    // perRow.style['border'] = 'solid #7e05ff 1px'
     perRow.style['width'] = '100%'
     perRow.style['height'] = '100px'
     
@@ -56,10 +76,37 @@ for(let i = 0; i < rowNum; i++){
 
 let goHover = document.querySelectorAll('div.box-item')
 
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
+//COLOR EVERY GRID BOX
 for(item of goHover){
 
-    item.addEventListener('mouseenter', event =>{
-        event.target.style['background-color'] = 'black'
-    })
+    item.addEventListener('mouseover', addColor)
+    item.addEventListener('mousedown', addColor)
 }
+
+function addColor(e){
+    if(e.type === 'mouseover' && mouseDown === true){
+        // if(mouseDown){
+        // 
+        // }
+        return e.target.style['background-color'] = 'black'
+    }
+}
+
+
+//RESET
+const rstBtn = document.createElement('button')
+
+rstBtn.textContent ='RESET BUTTON'
+rstBtn.style['margin-top'] ='20px'
+
+rstBtn.addEventListener('click', event =>{
+    
+    for(item of goHover){
+        item.style['background-color']='white'
+    }
+})
+chooseBody.appendChild(rstBtn)
